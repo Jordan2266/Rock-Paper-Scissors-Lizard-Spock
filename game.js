19a3206be51880e4d4b20c3ff07e12e6d1b0eb59
Jordan2266 <54198889+Jordan2266@users.noreply.github.com>
2019-11-25 08:23:51 -0500

    Start function code fix

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ var 2 = "paper"
 var 3 = "scissors"
 var 4 = "lizard"
 var 5 = "spock"
-var name = prompt("Enter chosen sign: ");
 
 
 
@@ -15,10 +14,10 @@ var name = prompt("Enter chosen sign: ");
 #4 = Lizard
 #5 = Spock
 
-<button onclick="myFunction()">Start</button>
+<button onclick="start()">Start</button>
 
 <script>
-function myfunction() {
+function start() {
 var user = prompt("Please choose (1) Rock, (2) Paper, (3) Scissors, (4) Lizard, or (5) Spock: ").upper()	
 var computerChoice = Math.floor(Math.random() * 5) + 1;
 if (computerChoice == 1) {
