7c867e8c6e66f160063cae18b04e949b6c79c8d9
Jordan2266 <54198889+Jordan2266@users.noreply.github.com>
2019-12-01 15:25:22 -0500

    Update game.js

--- game.js
+++ game.js
@@ -4,34 +4,62 @@ var 2 = "paper"
 var 3 = "scissors"
 var 4 = "lizard"
 var 5 = "spock"
+var choice1 = null
+var choice2 = null
+var userchoice = null
+var computerChoice = null
 
 
 <script>
-function start() {
-var user = prompt("Please choose (1) Rock, (2) Paper, (3) Scissors, (4) Lizard, or (5) Spock: ").upper()	
-var computerChoice = Math.floor(Math.random() * 5) + 1;
+function start() {	
+var userchoice = prompt("Please choose (1) Rock, (2) Paper, (3) Scissors, (4) Lizard, or (5) Spock: ").upper()
+if (userchoice == 1) {
+        userchoice == "rock";
+	set userchoice == choice1;
+} else if (userchoice == 2) {
+	userchoice == "paper";
+	set userchoice == choice1;
+} else if (userchoice == 3) {
+        userchoice == "scissors";
+	set userchoice == choice1;
+} else if (userchoice == 4) {
+        userchoice == "lizard";
+	set userchoice == choice1;
+} else if (userchoice == 5) {
+        userchoice == "spock";
+	set userchoice == choice1;
+</script>
+	
+<script>
+function start() {	
+var computerChoice = Math.floor(Math.random() * 5) + 1
 if (computerChoice == 1) {
     computerChoice = "rock";
+	set computerChoice == choice2;
 } else if (computerChoice == 2) {
     computerChoice = "paper";
+	set computerChoice == choice2;
 } else if (computerChoice == 3) {
     computerChoice = "scissors";
+	set computerChoice == choice2;
 } else if (computerChoice == 4) {
     computerChoice = "lizard";
+	set computerChoice == choice2;
 } else {
     computerChoice = "spock";
+	set computerChoice == choice2;
 }
 </script>
 
 	
 //Rock
 <scrpit>	
-else if (choice1 === "rock") {
-    if (choice2 === "scissors") {
+else if (choice1 == "rock") {
+    if (choice2 == "scissors") {
         alert("Rock wins!");
-    } else if (choice2 === "paper") {
+    } else if (choice2 == "paper") {
         alert("Paper wins!");
-    } else if (choice2 === "lizard") {
+    } else if (choice2 == "lizard") {
         alert("Rock wins!");
     } else {
         alert("Spock wins!");
@@ -41,12 +69,12 @@ else if (choice1 === "rock") {
 	
 //Paper
 <Script>	
-else if (choice1 === "paper") {
-    if (choice2 === "scissors") {
+else if (choice1 == "paper") {
+    if (choice2 == "scissors") {
         alert("Scissors wins!");
-    } else if (choice2 === "rock") {
+    } else if (choice2 == "rock") {
         alert("Paper wins!");
-    } else if (choice2 === "lizard") {
+    } else if (choice2 == "lizard") {
         alert("Lizard wins!");
     } else {
         alert("Paper wins!");
@@ -56,12 +84,12 @@ else if (choice1 === "paper") {
 	
 //Scissors
 <script>
-else if (choice1 === "scissors") {
-    if (choice2 === "paper") {
+else if (choice1 == "scissors") {
+    if (choice2 == "paper") {
         alert("Scissors wins!");
-    } else if (choice2 === "rock") {
+    } else if (choice2 == "rock") {
         alert("Rock wins!");
-    } else if (choice2 === "lizard") {
+    } else if (choice2 == "lizard") {
         alert("Scissors wins!");
     } else {
         alert("Spock wins!");
@@ -71,12 +99,12 @@ else if (choice1 === "scissors") {
 	
 //Lizard
 <script>	
-else if (choice1 === "lizard") {
-    if (choice2 === "scissors") {
+else if (choice1 == "lizard") {
+    if (choice2 == "scissors") {
         alert("Scissors wins!");
-    } else if (choice2 === "rock") {
+    } else if (choice2 == "rock") {
         alert("Rock wins!");
-    } else if (choice2 === "paper") {
+    } else if (choice2 == "paper") {
         alert("Lizard wins!");
     } else {
         alert("Lizard wins!");
@@ -86,12 +114,12 @@ else if (choice1 === "lizard") {
 	
 //Spock
 <script>	
-else if (choice1 === "spock") {
-    if (choice2 === "scissors") {
+else if (choice1 == "spock") {
+    if (choice2 == "scissors") {
         alert("Spock wins!");
-    } else if (choice2 === "rock") {
+    } else if (choice2 == "rock") {
         alert("Spock wins!");
-    } else if (choice2 === "lizard") {
+    } else if (choice2 == "lizard") {
         alert("Lizard wins!");
     } else {
         alert("Paper wins!");
