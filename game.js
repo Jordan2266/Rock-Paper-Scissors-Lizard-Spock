5ce35f60831e0d2ff43aac439ee73433c8680541
Jordan2266 <54198889+Jordan2266@users.noreply.github.com>
2019-12-01 11:07:49 -0500

    Computer 1 Logic

--- game.js
+++ game.js
@@ -6,3 +6,22 @@ var 4 = "lizard"
 var 5 = "spock"
 
 
+<script>
+function start() {
+var user = prompt("Please choose (1) Rock, (2) Paper, (3) Scissors, (4) Lizard, or (5) Spock: ").upper()	
+var computerChoice = Math.floor(Math.random() * 5) + 1;
+if (computerChoice == 1) {
+    computerChoice = "rock";
+} else if (computerChoice == 2) {
+    computerChoice = "paper";
+} else if (computerChoice == 3) {
+    computerChoice = "scissors";
+} else if (computerChoice == 4) {
+    computerChoice = "lizard";
+} else {
+    computerChoice = "spock";
+}
+</script>
+
+	
+	
